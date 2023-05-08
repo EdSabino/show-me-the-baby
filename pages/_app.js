cb4ebb5abf1f4c3250ac4687cc970bddd039ff5c
EdSabino <eduardoaikin@gmail.com>
2023-05-08 17:08:51 -0300

    t

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,10 @@
+process.env.AXIOM_TOKEN = process.env.NEXT_PUBLIC_AXIOM_TOKEN
+process.env.AXIOM_DATASET = process.env.NEXT_PUBLIC_AXIOM_DATASET
 import RouteGuard from '../components/routeGuard';
 import '../global.scss';
 export { reportWebVitals } from 'next-axiom';
 
 function MyApp({ Component, pageProps }) {
-  process.env.AXIOM_TOKEN = process.env.NEXT_PUBLIC_AXIOM_TOKEN
-  process.env.AXIOM_DATASET = process.env.NEXT_PUBLIC_AXIOM_DATASET
   return <RouteGuard>
     <Component {...pageProps} />
   </RouteGuard>
